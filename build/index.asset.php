1c7e83fe5bdf5bc04cad60a6b6b70fbeda33138f
Tek Raj Bhatt <tekbhatt1@gmail.com>
2024-08-30 23:34:17 +0545

    Modified the block files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f5e6b814e1de75768b5b381e22df3b7f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '96061b7f4b82bba6f0e8');
